b40a7f8435c509f1d530e2391d60f3a4796acd9d
zcj924 <zcj924@gmail.com>
2019-10-25 11:55:42 +0800

    可以读出拍数

--- kirakiraBeep/kirakiraBeep.cpp
+++ kirakiraBeep/kirakiraBeep.cpp
@@ -1,24 +1,45 @@
 ﻿#include <stdio.h>
 #include <Windows.h>
 #include "scale.h"
+#define MAXSIZE 84998//定义最大个数(VS2019通过测试)
 
-int qu, er, si, liu, ba, sl;
-int BPM(int p) {
-	qu = p / 60;//全音符
-	er = qu / 2;//2分音符 
-	si = er / 2;//4分音符
-	liu = er + si;//6分音符
-	ba = si / 2;//8分音符
-	sl = ba / 2;//16分音符
-	return p;
+int i,j,p,fen,ms;
+char buffer[MAXSIZE];
+
+int BPM() {//计算排数，p
+	if (buffer[i + 1] >= '0' && buffer[i + 1] <= '9')
+	{
+		p = (buffer[i + 1] - '0') * 100;
+	}
+	if (buffer[i + 2] >= '0' && buffer[i + 2] <= '9')
+	{
+		p = p + (buffer[i + 2] - '0') * 10;
+	}
+	if (buffer[i + 3] >= '0' && buffer[i + 3] <= '9')
+	{
+		p = p + (buffer[i + 3] - '0');
+	}
+	p = p * 1000;
 }
+int FQ() {//通过排数 p ，计算实际毫秒数 ms 
+	if (buffer[i + 1] >= '0' && buffer[i + 1] <= '9')
+	{
+		fen = (buffer[i + 1] - '0') * 10;
+	}
+	if (buffer[i + 2] >= '0' && buffer[i + 2] <= '9')
+	{
+		fen = p + (buffer[i + 2] - '0');
+	}
+	if (fen = 1) ms = p / 60;//全音符
+	if (fen = 2) ms = p / 120;//2分音符
+	if (fen = 4) ms = p / 240;//4分音符
+	if (fen = 6) ms = (p / 120) + (p / 240);//6分音符
+	if (fen = 8) ms = p / 480;//8分音符
+	if (fen = 16) ms = p / 960;//16分音符
 
+}
 int main(){
-	int p = 60;
-	BPM(p * 1000);
-
 	FILE* fp;
-	char buffer[1000];
 	fopen_s(&fp, "新建文本文档.txt", "r");
 	if (fp == NULL)
 	{
@@ -29,20 +50,22 @@ int main(){
 	while (!feof(fp))//循环读取每一行，直到文件尾
 	{
 		fgets(buffer, 1000, fp);//将fp所指向的文件一行内容读到缓冲区
-		printf("%s\n", buffer);//输出所读到的内容
-		for (size_t j = 0; buffer[j] == '\0'; j++)
+		while (buffer[j] != '\0')//求数组结尾
+		{
+			j++;
+		}
 		for (size_t i = 0; i<j; i++)
 		{
-			if (buffer[i] == 'P') {
-
+			if (buffer[i] == 'P') {//判断 P 的位置，向下3个元素
+				BPM();
 			}
-			
-			printf("%d\n", i);
+			if (buffer[i] == ' ') {//判断 “空格” 的位置，向下2个元素
+				FQ();
 		}
-		
-		   					
 
 	}
+
+
 	fclose(fp);
 	
 
