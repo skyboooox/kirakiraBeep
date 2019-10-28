8f2c7789e1f248c14108c5d7f4be647d4bec488f
zcj924 <zcj924@gmail.com>
2019-10-28 11:31:44 +0800

    基本功能完成

--- kirakiraBeep/kirakiraBeep.cpp
+++ kirakiraBeep/kirakiraBeep.cpp
@@ -1,48 +1,144 @@
 ﻿#include <stdio.h>
 #include <Windows.h>
-#include "scale.h"
 #define MAXSIZE 84998//定义最大个数(VS2019通过测试)
 
-int i,j,p,fen,ms;
+int p,fen,ms,fq;
 char buffer[MAXSIZE];
 
-int BPM() {//计算排数，p
-	if (buffer[i + 1] >= '0' && buffer[i + 1] <= '9')
-	{
+int BPM(int i) {//计算拍数，p
+	if (buffer[i + 1] >= '0' && buffer[i + 1] <= '9') {
 		p = (buffer[i + 1] - '0') * 100;
 	}
-	if (buffer[i + 2] >= '0' && buffer[i + 2] <= '9')
-	{
+	if (buffer[i + 2] >= '0' && buffer[i + 2] <= '9') {
 		p = p + (buffer[i + 2] - '0') * 10;
 	}
-	if (buffer[i + 3] >= '0' && buffer[i + 3] <= '9')
-	{
+	if (buffer[i + 3] >= '0' && buffer[i + 3] <= '9') {
 		p = p + (buffer[i + 3] - '0');
 	}
 	p = p * 1000;
+	return(1);
 }
-int FQ() {//通过排数 p ，计算实际毫秒数 ms 
+
+int TIME(int i) {//通过拍数 p ，计算实际毫秒数 ms 
 	if (buffer[i + 1] >= '0' && buffer[i + 1] <= '9')
 	{
 		fen = (buffer[i + 1] - '0') * 10;
 	}
 	if (buffer[i + 2] >= '0' && buffer[i + 2] <= '9')
 	{
-		fen = p + (buffer[i + 2] - '0');
+		fen = fen + (buffer[i + 2] - '0');
 	}
-	if (fen = 1) ms = p / 60;//全音符
-	if (fen = 2) ms = p / 120;//2分音符
-	if (fen = 4) ms = p / 240;//4分音符
-	if (fen = 6) ms = (p / 120) + (p / 240);//6分音符
-	if (fen = 8) ms = p / 480;//8分音符
-	if (fen = 16) ms = p / 960;//16分音符
+	if (fen == 1) ms = p / 60;//全音符
+	if (fen == 2) ms = p / 120;//2分音符
+	if (fen == 4) ms = p / 240;//4分音符
+	if (fen == 6) ms = (p / 120) + (p / 240);//6分音符
+	if (fen == 8) ms = p / 480;//8分音符
+	if (fen == 16) ms = p / 960;//16分音符
+	return(1);
 
 }
-int main(){
+int FQ(int i) {//频率
+	if (buffer[i + 3] == 'C') {
+		if (buffer[i + 4] == '1') fq = 130;
+		if (buffer[i + 4] == '2') fq = 261;
+		if (buffer[i + 4] == '3') fq = 523;
+		if (buffer[i + 4] == '4') fq = 1046;
+		if (buffer[i + 4] == '5') fq = 2093;
+			if (buffer[i + 4] == '#') {
+				if (buffer[i + 5] == '1') fq = 138;
+				if (buffer[i + 5] == '2') fq = 227;
+				if (buffer[i + 5] == '3') fq = 554;
+				if (buffer[i + 5] == '4') fq = 1108;
+				if (buffer[i + 5] == '5') fq = 2217;
+			}
+
+	}
+		if (buffer[i + 3] == 'D') {
+			if (buffer[i + 4] == '1') fq = 147;
+			if (buffer[i + 4] == '2') fq = 294;
+			if (buffer[i + 4] == '3') fq = 587;
+			if (buffer[i + 4] == '4') fq = 1175;
+			if (buffer[i + 4] == '5') fq = 2349;
+			if (buffer[i + 4] == '#') {
+				if (buffer[i + 5] == '1') fq = 155;
+				if (buffer[i + 5] == '2') fq = 311;
+				if (buffer[i + 5] == '3') fq = 622;
+				if (buffer[i + 5] == '4') fq = 1244;
+				if (buffer[i + 5] == '5') fq = 2489;
+			}
+
+	}
+
+	if (buffer[i + 3] == 'E') {
+		if (buffer[i + 4] == '1') fq = 164;
+		if (buffer[i + 4] == '2') fq = 327;
+		if (buffer[i + 4] == '3') fq = 659;
+		if (buffer[i + 4] == '4') fq = 1318;
+		if (buffer[i + 4] == '5') fq = 2637;
+	}
+
+	if (buffer[i + 3] == 'F') {
+		if (buffer[i + 4] == '1') fq = 174;
+		if (buffer[i + 4] == '2') fq = 349;
+		if (buffer[i + 4] == '3') fq = 698;
+		if (buffer[i + 4] == '4') fq = 1396;
+		if (buffer[i + 4] == '5') fq = 2793;
+			if (buffer[i + 4] == '#') {
+				if (buffer[i + 5] == '1') fq = 185;
+				if (buffer[i + 5] == '2') fq = 369;
+				if (buffer[i + 5] == '3') fq = 740;
+				if (buffer[i + 5] == '4') fq = 1480;
+				if (buffer[i + 5] == '5') fq = 2960;
+			}
+			i = i + 4;
+	}
+
+	if (buffer[i + 3] == 'G') {
+		if (buffer[i + 4] == '1') fq = 196;
+		if (buffer[i + 4] == '2') fq = 392;
+		if (buffer[i + 4] == '3') fq = 783;
+		if (buffer[i + 4] == '4') fq = 1568;
+		if (buffer[i + 4] == '5') fq = 3136;
+			if (buffer[i + 4] == '#') {
+				if (buffer[i + 5] == '1') fq = 207;
+				if (buffer[i + 5] == '2') fq = 415;
+				if (buffer[i + 5] == '3') fq = 830;
+				if (buffer[i + 5] == '4') fq = 1661;
+				if (buffer[i + 5] == '5') fq = 3322;
+			}
+
+	}
+	if (buffer[i + 3] == 'A') {
+		if (buffer[i + 4] == '1') fq = 220;
+		if (buffer[i + 4] == '2') fq = 440;
+		if (buffer[i + 4] == '3') fq = 880;
+		if (buffer[i + 4] == '4') fq = 1760;
+		if (buffer[i + 4] == '5') fq = 3520;
+			if (buffer[i + 4] == '#') {
+				if (buffer[i + 5] == '1') fq = 223;
+				if (buffer[i + 5] == '2') fq = 466;
+				if (buffer[i + 5] == '3') fq = 932;
+				if (buffer[i + 5] == '4') fq = 1864;
+				if (buffer[i + 5] == '5') fq = 3729;
+			}
+
+	}
+	if (buffer[i + 3] == 'B') {
+		if (buffer[i + 4] == '1') fq = 246;
+		if (buffer[i + 4] == '2') fq = 493;
+		if (buffer[i + 4] == '3') fq = 987;
+		if (buffer[i + 4] == '4') fq = 1975;
+		if (buffer[i + 4] == '5') fq = 3951;
+	}
+	if (buffer[i + 3] == 'S') 	fq = 1;
+	i = i + 4;
+	return(1);
+}
+
+int main() {
 	FILE* fp;
 	fopen_s(&fp, "新建文本文档.txt", "r");
-	if (fp == NULL)
-	{
+	if (fp == NULL) {
 		printf("不能打开 \n");
 		exit(1);
 	}
@@ -50,24 +146,28 @@ int main(){
 	while (!feof(fp))//循环读取每一行，直到文件尾
 	{
 		fgets(buffer, 1000, fp);//将fp所指向的文件一行内容读到缓冲区
+		size_t j=0;
 		while (buffer[j] != '\0')//求数组结尾
 		{
 			j++;
 		}
-		for (size_t i = 0; i<j; i++)
-		{
+		for (size_t i = 0; i < j-2; i++) {
 			if (buffer[i] == 'P') {//判断 P 的位置，向下3个元素
-				BPM();
+				BPM(i);
 			}
+			i = i + 4;
 			if (buffer[i] == ' ') {//判断 “空格” 的位置，向下2个元素
-				FQ();
+				TIME(i);
+			}
+			
+			FQ(i);
+			Beep(fq, ms);
+			fq = 0, ms = 0;
 		}
-
 	}
 
-
 	fclose(fp);
-	
+
 
 	return 0;
 }
--- kirakiraBeep/scale.h
+++ kirakiraBeep/scale.h
@@ -1,64 +1,125 @@
-//#define C5 4186
-//#define C2S 4440;
-//#define D2 
-//#define D2S 
-//#define E2 
-//#define F2
-//#define F2S
-//#define G2
-//#define G2S
-//#define A2
-//#define A2S
-//#define B2
-//
-//#define C2
-//#define C2S
-//#define D2
-//#define D2S
-//#define E2
-//#define F2
-//#define F2S
-//#define G2
-//#define G2S
-//#define A2
-//#define A2S
-//#define B2
-//
-//#define C3
-//#define C3S
-//#define D3
-//#define D3S
-//#define E3
-//#define F3
-//#define F3S
-//#define G3
-//#define G3S
-//#define A3
-//#define A3S
-//#define B3
-//
-//#define C2
-//#define C2S
-//#define D2
-//#define D2S
-//#define E2
-//#define F2
-//#define F2S
-//#define G2
-//#define G2S
-//#define A2
-//#define A2S
-//#define B2
-//
-//#define C
-//#define CS
-//#define D
-//#define DS
-//#define E
-//#define F
-//#define FS
-//#define G
-//#define GS
-//#define A
-//#define AS
-//#define B
+int BPM(int i) {//计算拍数，p
+	if (buffer[i + 1] >= '0' && buffer[i + 1] <= '9') {
+		p = (buffer[i + 1] - '0') * 100;
+	}
+	if (buffer[i + 2] >= '0' && buffer[i + 2] <= '9') {
+		p = p + (buffer[i + 2] - '0') * 10;
+	}
+	if (buffer[i + 3] >= '0' && buffer[i + 3] <= '9') {
+		p = p + (buffer[i + 3] - '0');
+	}
+	p = p * 1000;
+	return(1);
+}
+
+int TIME(int i) {//通过拍数 p ，计算实际毫秒数 ms 
+	if (buffer[i + 1] >= '0' && buffer[i + 1] <= '9') {
+		fen = (buffer[i + 1] - '0') * 10;
+	}
+	if (buffer[i + 2] >= '0' && buffer[i + 2] <= '9') {
+		fen = fen + (buffer[i + 2] - '0');
+	}
+	if (fen == 1) ms = p / 60;//全音符
+	if (fen == 2) ms = p / 120;//2分音符
+	if (fen == 4) ms = p / 240;//4分音符
+	if (fen == 6) ms = (p / 120) + (p / 240);//6分音符
+	if (fen == 8) ms = p / 480;//8分音符
+	if (fen == 16) ms = p / 960;//16分音符
+	return(1);
+
+}
+int FQ(int i) {
+	if (buffer[i + 3] == 'C') {
+		if (buffer[i + 4] == '1') fq = 130;
+		if (buffer[i + 4] == '2') fq = 261;
+		if (buffer[i + 4] == '3') fq = 523;
+		if (buffer[i + 4] == '4') fq = 1046;
+		if (buffer[i + 4] == '5') fq = 2093;
+		if (buffer[i + 4] == '#') {
+			if (buffer[i + 4] == '1') fq = 138;
+			if (buffer[i + 4] == '2') fq = 227;
+			if (buffer[i + 4] == '3') fq = 554;
+			if (buffer[i + 4] == '4') fq = 1108;
+			if (buffer[i + 4] == '5') fq = 2217;
+		}
+
+	}
+	if (buffer[i + 3] == 'D') {
+		if (buffer[i + 4] == '1') fq = 147;
+		if (buffer[i + 4] == '2') fq = 294;
+		if (buffer[i + 4] == '3') fq = 587;
+		if (buffer[i + 4] == '4') fq = 1175;
+		if (buffer[i + 4] == '5') fq = 2349;
+		if (buffer[i + 4] == '#') {
+			if (buffer[i + 4] == '1') fq = 155;
+			if (buffer[i + 4] == '2') fq = 311;
+			if (buffer[i + 4] == '3') fq = 622;
+			if (buffer[i + 4] == '4') fq = 1244;
+			if (buffer[i + 4] == '5') fq = 2489;
+		}
+
+	}
+
+	if (buffer[i + 3] == 'E') {
+		if (buffer[i + 4] == '1') fq = 164;
+		if (buffer[i + 4] == '2') fq = 327;
+		if (buffer[i + 4] == '3') fq = 659;
+		if (buffer[i + 4] == '4') fq = 1318;
+		if (buffer[i + 4] == '5') fq = 2637;
+	}
+
+	if (buffer[i + 3] == 'F') {
+		if (buffer[i + 4] == '1') fq = 174;
+		if (buffer[i + 4] == '2') fq = 349;
+		if (buffer[i + 4] == '3') fq = 698;
+		if (buffer[i + 4] == '4') fq = 1396;
+		if (buffer[i + 4] == '5') fq = 2793;
+		if (buffer[i + 4] == '#') {
+			if (buffer[i + 4] == '1') fq = 185;
+			if (buffer[i + 4] == '2') fq = 369;
+			if (buffer[i + 4] == '3') fq = 740;
+			if (buffer[i + 4] == '4') fq = 1480;
+			if (buffer[i + 4] == '5') fq = 2960;
+		}
+
+	}
+
+	if (buffer[i + 3] == 'G') {
+		if (buffer[i + 4] == '1') fq = 196;
+		if (buffer[i + 4] == '2') fq = 392;
+		if (buffer[i + 4] == '3') fq = 783;
+		if (buffer[i + 4] == '4') fq = 1568;
+		if (buffer[i + 4] == '5') fq = 3136;
+		if (buffer[i + 4] == '#') {
+			if (buffer[i + 4] == '1') fq = 207;
+			if (buffer[i + 4] == '2') fq = 415;
+			if (buffer[i + 4] == '3') fq = 830;
+			if (buffer[i + 4] == '4') fq = 1661;
+			if (buffer[i + 4] == '5') fq = 3322;
+		}
+
+	}
+	if (buffer[i + 3] == 'A') {
+		if (buffer[i + 4] == '1') fq = 220;
+		if (buffer[i + 4] == '2') fq = 440;
+		if (buffer[i + 4] == '3') fq = 880;
+		if (buffer[i + 4] == '4') fq = 1760;
+		if (buffer[i + 4] == '5') fq = 3520;
+		if (buffer[i + 4] == '#') {
+			if (buffer[i + 4] == '1') fq = 223;
+			if (buffer[i + 4] == '2') fq = 466;
+			if (buffer[i + 4] == '3') fq = 932;
+			if (buffer[i + 4] == '4') fq = 1864;
+			if (buffer[i + 4] == '5') fq = 3729;
+		}
+
+	}
+	if (buffer[i + 3] == 'B') {
+		if (buffer[i + 4] == '1') fq = 246;
+		if (buffer[i + 4] == '2') fq = 493;
+		if (buffer[i + 4] == '3') fq = 987;
+		if (buffer[i + 4] == '4') fq = 1975;
+		if (buffer[i + 4] == '5') fq = 3951;
+	}
+	return(1);
+}
